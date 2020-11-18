9c84dc74a47af2fccd5517ef093fab05389fc5e6
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-11-18 09:40:24 +0800

    Create 111801.自定义.c

---
+++ 111801.自定义.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	int a = 3, b = 4, c = 5, x, y;
+	scanf("%d", &a);
+	if (a < 0) { 
+		printf("你还有花呗没还，无法使用该程序。\n");
+	} else if (a <= 500) {
+		printf("赤贫\n");
+	} else if (a <= 1500) {
+		printf("贫困\n");
+	} else if (a <= 3000) {
+		printf("普通打工人。\n");
+	} else if (a <= 5000) {
+		printf("普通清洁工。\n");
+	} else if (a <= 12000) {
+		printf("水电工工资。\n");
+	} else if (a <= 50000) {
+		printf("脑袋大脖子粗不是老板就是伙夫。\n");
+	} else {
+		printf("程序员。\n");
+	}
+}
