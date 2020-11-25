c5d6151480e428f963d84aa1b28b4c6995aa3b08
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-11-25 09:27:39 +0800

    Create 112501.打工人工资.app.c

---
+++ 112501.打工人工资.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a;
+	scanf("%d", &a);
+	if (a < 0 || a > 20000) { 
+		printf("其他，输入有误，请重新输入。\n");
+	} else if (a <= 100) {
+		printf("贫困打工人\n");
+	} else if (a <= 1000) {
+		printf("一般打工人\n");
+	} else if (a <= 20000) {
+		printf("土豪打工人。\n");
+	}
+}
