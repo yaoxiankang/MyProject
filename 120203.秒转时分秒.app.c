ae8ab156b84f3ac28f06bf3b510796969d3e3377
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-12-02 16:34:29 +0800

    Create 120203.秒转时分秒.app.c

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+{
+void main()
+int h, m, s;
+scanf("%d", &s):
+h = s / 3600;
+m = s / 60;
+m = m > 60 ? m - h * 60： m；
+s = s > 60 ？ s - h * 3600 - m * 60 ： s；
+printf("%d: %d:%d ", h, m, s)；
+}
