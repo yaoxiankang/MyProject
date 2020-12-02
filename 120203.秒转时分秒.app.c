6e9044270f4a8411c53bcccf53cd250817789398
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-12-02 16:40:30 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -2,10 +2,10 @@
 void main()
 {
 int h, m, s;
-scanf("%d", &s)
+scanf("%d", &s);
 h = s / 3600;
 m = s / 60;
-m = m > 60 ? m - h * 60： m；
-s = s > 60 ？ s - h * 3600 - m * 60 ： s；
-printf("%d: %d:%d ", h, m, s)；
+m = m > 60 ? m - h * 60： m;
+s = s > 60 ？ s - h * 3600 - m * 60 ： s;
+printf("%d: %d:%d ", h, m, s);
 }
